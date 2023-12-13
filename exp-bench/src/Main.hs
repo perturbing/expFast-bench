ad6e19364ef606d68ab28f918be413b8f282f5c9
perturbing <thomas.vellekoop@iohk.io>
2023-12-13 07:50:49 +0100

    add elaborate test bases on different sizes of inputs for expFast

--- exp-bench/src/Main.hs
+++ exp-bench/src/Main.hs
@@ -1,38 +1,65 @@
 module Main where
 
 import Criterion.Main
+import Criterion.Types
 import Crypto.Number.ModArithmetic (expFast)
 import Crypto.Random (getRandomBytes)
+import Crypto.Number.Prime (generatePrime)
 import Crypto.Number.Serialize (os2ip)
 import Data.ByteString (ByteString)
+import Control.Monad (replicateM)
 
--- Function to generate a random integer of a given byte size
 randomInteger :: Int -> IO Integer
 randomInteger size = os2ip <$> (getRandomBytes size :: IO ByteString)
 
--- Setup environment for benchmarks
-setupEnv :: Integer -> Int -> IO (Integer, Integer, Integer)
-setupEnv modulus modulusSize = do
-    base <- randomInteger modulusSize
-    exponent <- randomInteger modulusSize
-    return (base, exponent, modulus)
+setupEnv :: Int -> Int -> Int -> IO (Integer, Integer, Integer)
+setupEnv pSizeBits bSize eSize = do
+    if bSize < 0
+        then if eSize < 0
+            then do
+                modulus <- generatePrime pSizeBits
+                base <- randomInteger (abs bSize)
+                exponent <- randomInteger (abs eSize)
+                return (-base, -exponent, modulus)
+            else do
+                modulus <- generatePrime pSizeBits
+                base <- randomInteger (abs bSize)
+                exponent <- randomInteger eSize
+                return (-base, exponent, modulus)
+        else if eSize < 0
+            then do
+                modulus <- generatePrime pSizeBits
+                base <- randomInteger bSize
+                exponent <- randomInteger (abs eSize)
+                return (base, exponent, modulus)
+        else
+            do modulus <- generatePrime pSizeBits
+               base <- randomInteger bSize
+               exponent <- randomInteger eSize
+               return (base, exponent, modulus)
 
--- Benchmark for expFast with specific modulus size
 modExpFastBenchmark :: Int -> (Integer, Integer, Integer) -> Benchmark
 modExpFastBenchmark modulusSize (base, exponent, modulus) =
-    bench ("expFast of " ++ show modulusSize ++ " bytes") $ 
+    bench (show modulusSize ++ " bytes") $
     whnf (expFast base exponent) modulus
+
+writeBench :: Int -> Int -> Int -> IO ()
+writeBench psize bsize esize = do
+    let myConfig = defaultConfig { csvFile = Just ("data/"<>show psize <> "/benchmark_results_"<> show bsize <> "_" <> show esize <> ".csv"), verbosity = Quiet }
+    env <- replicateM 10 (setupEnv psize bsize esize)
+    defaultMainWith myConfig [
+        bgroup "pSize: " (map (modExpFastBenchmark psize) env)
+        ]
+
+writeBenchResults :: Int -> IO ()
+writeBenchResults psize = do
+    let psizeBits = psize * 8
+    let list = [ (x,y) | x <- [-64, -48 .. 64], y <- [-64, -48 .. 64]]
+    mapM_ (uncurry (writeBench psizeBits)) list
 
 main :: IO ()
 main = do
-  -- Pre-generate random numbers for each modulus size
-  env256 <- setupEnv 52435875175126190479447740508185965837690552500527637822603658699938581184513 32
-  env381 <- setupEnv 4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559787 48
-
-  -- Run benchmarks
-  defaultMain [
-    bgroup "expFast" [
-      modExpFastBenchmark 32 env256,
-      modExpFastBenchmark 48 env381
-        ]
-    ]
+    writeBenchResults 16
+    writeBenchResults 32
+    writeBenchResults 48
+    writeBenchResults 64
